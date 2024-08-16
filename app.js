9e348b6adb3df41e79d66967b8799bc512889ba3
Giacomo Radaelli <gradaellig@protonmail.com>
2024-08-16 16:54:01 +0200

    Added status check for services, some UI enhancements

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let HOST
 
-
 let servicesArray = [];
-
 
 function validateSettings(data) {
     if (data.host == null) {
@@ -25,6 +23,28 @@ function loadData(file, callback) {
         .catch(error => console.error('Data loading error', error));
 }
 
+// As there can be lots of services, we need to check their status asynchronously
+async function checkServiceStatus(service) {
+	try {
+		// Simple check, might not work for all services but for mine it's enough
+		const response = await fetch(service.url, {
+            method: 'HEAD',
+            mode: 'no-cors'
+        });
+
+		// If there is no error, the service is online
+		document.getElementById(`status-${service.id}`).innerHTML = `
+			<img class="status-indicator" src="./images/status/online.svg"></span>
+			<p class="status-text">Online</p>
+		`;
+	} catch (error) {
+		document.getElementById(`status-${service.id}`).innerHTML = `
+			<img class="status-indicator" src="./images/status/offline.svg"></span>
+			<p class="status-text">Offline</p>
+		`;
+	}
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const content = document.getElementById('content');
 
@@ -33,6 +53,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		try {
 			validateSettings(data);
 			if(data.host) HOST = data.host;
+
+			if(data.background_img_path) {
+				document.body.style.backgroundImage = `url(${data.background_img_path})`;
+				document.body.style.backgroundSize = "cover";
+			}
+
 		} catch (error) {
 			console.error("Error in settings validation: ", error);
 		}
@@ -41,8 +67,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // Loads services
     loadData("conf/services.json", function(data) {
 		try {
-			data.services.forEach((item) => {
+			validateServices(data);
+
+			data.services.forEach((item, index) => {
 				let service = {
+					id: index,
 					title: item.title,
 					port: item.port,
 					image: item.image,
@@ -53,28 +82,34 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 			// Sort services by port
 			servicesArray.sort((a, b) => a.port - b.port);
-	
+
+			console.log("If you see some errors below, it's because the services are offline. It's normal.");
 			servicesArray.forEach(service => {
 				let serviceElement = createServiceElement(service);
 				content.appendChild(serviceElement);
+				checkServiceStatus(service);
 			});
-
-			validateServices(data);
 		} catch (error) {
 			console.error("Error in services validation: ", error);
 		}
     });
+
+
 });
 
 function createServiceElement(service) {
     let element = document.createElement('a');
 	element.href = service.url;
-    element.className = 'item';
+    element.className = 'item hvr-grow hvr-fade';
 
     element.innerHTML = `
 		<img src="${service.image}" alt="${service.title} logo" class="service-logo">
 		<h2>${service.title}</h2>
 		<p>Port ${service.port}</p>
+		<div id="status-${service.id}" class="status-div">
+			<img class="status-indicator" src="./images/status/offline.svg"></span>
+			<p class="status-text">Offline</p>
+		</div>
     `;
 
     return element;
